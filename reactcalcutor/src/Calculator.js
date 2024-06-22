51a2240cc6cf66474941cbfea8c56bbdaba30b5f
Matilda Söderberg <matilda.89@live.se>
2024-06-22 16:26:11 +0200

    Numbers display on input + added CE and C

--- reactcalcutor/src/Calculator.js
+++ reactcalcutor/src/Calculator.js
@@ -1,26 +1,36 @@
 ﻿import './calculator.css';
+import { useState } from 'react';
 
 const Calculator = () => {
 
-    {
-        /* Function to print all buttons? Maybe a loop */
-
-    }
     const numberButtons = [];
+    const [number, setNumber] = useState("");
+    const handleClick = (e) => {
+        const value = e.target.value;
+        if (value == "C") {
+            setNumber("");
+        } else if (value == "CE") {
+            setNumber(prevNumber => prevNumber.slice(0, 0 - 1));
+        }else {
+        setNumber(prevNumber => prevNumber + value);
 
-    for (let i = 0; i < 12; i++) {
-        numberButtons.push(<button key={i}>{i}</button>);
+        }
+    };
+    for (let i = 0; i < 10; i++) {
+        numberButtons.push(<button key={i} onClick={handleClick} value={i}>{i}</button>);
     }
     return (
         <>
             <div className="math-container">
                 <p>Happy counting! (•‿•) </p>
-                    <input type="string" /> {/* Display Result */}
+                <input type="string" value={number} readOnly /> {/* Display Result */}
                 <div className="number-container">
-                    <button>+</button>
-                    <button>-</button>
-                    <button>=</button>
-                    <button>.</button>
+                    <button onClick={handleClick} value={"+"}>+</button>
+                    <button onClick={handleClick} value={"-"}>-</button>
+                    <button onClick={handleClick} value={"="}>=</button>
+                    <button onClick={handleClick} value={"."}>.</button>
+                    <button onClick={handleClick} value={"C"}>CE</button>
+                    <button onClick={handleClick} value={"CE"}>C</button>
                     {numberButtons}
                 </div>
             </div>
