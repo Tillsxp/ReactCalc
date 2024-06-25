6f0ff1708197eb98003b0ec977084367252ea650
Matilda Söderberg <matilda.89@live.se>
2024-06-25 11:39:28 +0200

    Rainbow text added

--- reactcalcutor/src/Calculator.js
+++ reactcalcutor/src/Calculator.js
@@ -7,27 +7,30 @@ const Calculator = () => {
     const [number, setNumber] = useState("");
     const handleClick = (e) => {
         const value = e.target.value;
-        if (value == "C") {
-            setNumber("");
-        } else if (value == "CE") {
-            setNumber(prevNumber => prevNumber.slice(0, 0 - 1));
-        }else {
-        setNumber(prevNumber => prevNumber + value);
 
-        }
+        return value == "C" ? setNumber("")
+            : value == "CE" ? setNumber(prevNumber => prevNumber.slice(0, 0 - 1))
+                : setNumber(prevNumber => prevNumber + value); {}
+        const numberValue = parseInt(number, 10);
+        number = numberValue;
+        console.log(number);
     };
     for (let i = 0; i < 10; i++) {
         numberButtons.push(<button key={i} onClick={handleClick} value={i}>{i}</button>);
     }
+
+    const handleMatch = (e) => {
+        {/* Count Math if = is pressed*/ }
+    }
     return (
         <>
             <div className="math-container">
-                <p>Happy counting! (•‿•) </p>
+                <p className="rainbow">Happy counting! (•‿•) </p>
                 <input type="string" value={number} readOnly /> {/* Display Result */}
                 <div className="number-container">
                     <button onClick={handleClick} value={"+"}>+</button>
                     <button onClick={handleClick} value={"-"}>-</button>
-                    <button onClick={handleClick} value={"="}>=</button>
+                    <button onClick={handleClick} >=</button> {/* Don't print = only show */ }
                     <button onClick={handleClick} value={"."}>.</button>
                     <button onClick={handleClick} value={"C"}>CE</button>
                     <button onClick={handleClick} value={"CE"}>C</button>
